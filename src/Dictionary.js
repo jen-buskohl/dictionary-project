770f1ecb1b584d32fcfa2a0a56d2d01d5f546701
jen-buskohl <124739446+jen-buskohl@users.noreply.github.com>
2023-10-24 14:41:53 -0700

    Added the search input functionality

---
+++ src/Dictionary.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState(null);
+
+  function search(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}...`);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input
+          type="search"
+          className="Dictionary-input"
+          onChange={handleKeywordChange}
+        />
+      </form>
+    </div>
+  );
+}
